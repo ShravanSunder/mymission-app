36e58de5388bada015eeb5219a8c1a5656849af2
Shravan Sunder <shravan.retail@gmail.com>
2021-02-05 08:38:18 -0500

    added extra tailwind plugins and updated config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 module.exports = {
-   purge: {
-      enabled: false,
-      content: ['./src/**/*.{jsx,tsx,html}', './src/index.html'],
-   },
+   purge: ['./src/**/*.{jsx,tsx,html}', './src/index.html'],
+   // purge: {
+   //    enabled: false,
+   //    content: ['./src/**/*.{jsx,tsx,html}', './src/index.html'],
+   // },
    darkMode: 'class', // or 'media' or 'class'
    important: false,
    corePlugins: {
       //preflight: false,
    },
-   plugins: [require('@tailwindcss/typography'), require('tailwindcss-elevation')(['responsive'])],
+   plugins: [
+      require('@tailwindcss/typography'),
+      require('tailwindcss-elevation')(['responsive']),
+      require('@tailwindcss/aspect-ratio'),
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/line-clamp'),
+   ],
 };

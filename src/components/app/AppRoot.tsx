6872bc785f7a639b09504ddedb3071c5f2ca94e0
Shravan Sunder <shravan.retail@gmail.com>
2021-03-10 09:45:25 -0500

    updated packages and added recoil / observable-hooks and removed redux-observable

--- src/components/app/AppRoot.tsx
+++ src/components/app/AppRoot.tsx
@@ -9,7 +9,12 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { MainAppLayout } from '../layout/MainAppLayout';
 import { ErrorBoundary, ErrorFallback } from '~~/components/common/ErrorFallback';
 
-const RootRoutes = (): JSX.Element => {
+/**
+ * App Root that also has the Root Routes required for navigation
+ * Intatiates ThemeProvider as well as a Suspense and Errorboundary
+ * @see ThemeProvider
+ */
+const AppRoot: React.FC = () => {
    return (
       <ThemeProvider>
          <BrowserRouter>
@@ -17,8 +22,8 @@ const RootRoutes = (): JSX.Element => {
                <ErrorBoundary FallbackComponent={ErrorFallback}>
                   <Routes>
                      <Route path="/" element={<MainAppLayout></MainAppLayout>} />
-                     <Route path="/users/*" element={<div>users path</div>}></Route>
-                     <Route path="/noworkie/:id" element={<div>dsfsdfsdf</div>}></Route>
+                     <Route path="/users/*" element={<div>test users path</div>}></Route>
+                     <Route path="/noworkie/:id" element={<div>test not workie</div>}></Route>
                      <Route path="/*" element={<div>notfound</div>} />
                   </Routes>
                </ErrorBoundary>
@@ -28,4 +33,4 @@ const RootRoutes = (): JSX.Element => {
    );
 };
 
-export default RootRoutes;
+export default AppRoot;

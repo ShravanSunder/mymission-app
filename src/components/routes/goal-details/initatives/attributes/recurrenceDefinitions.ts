a13ad7e6b3bca4820109e90bf36eaa5b29dd5623
Shravan Sunder <shravan.retail@gmail.com>
2021-03-10 21:56:01 -0500

    [ch27] changed definitions to properly format recurrence.  created a hook that provides intl strings with pularisation

---
+++ src/components/routes/goal-details/initatives/attributes/recurrenceDefinitions.ts
@@ -0,0 +1,123 @@
+import { useIntl } from 'react-intl';
+import { createException, ExceptionTypes } from '~~/models/Exception';
+import { DaysOfWeek } from './scheduleDefinitions';
+import dayjs from 'dayjs';
+import weekday from 'dayjs/plugin/weekday';
+
+/**
+ * The period for which you are aggregating (counting) your habit?
+ * - most common: total count for the day (aggregate over a day)
+ * - aggregate over a week
+ * - aggregate over a month
+ * - aggegate over a quarter
+ */
+export enum RecurrenceAggregationPeriods {
+   TotalPerDay = 'day',
+   TotalPerWeek = 'week',
+   TotalPerMonth = 'month',
+   TotalPerQuarter = 'quarter',
+}
+/**
+ * How long is the duration of time over which you are tracking your habit
+ * for example?
+ * - Number of successfull days a week?
+ * - Number of successfull days a month
+ * - Number of successful weeks a month?
+ */
+
+export enum RecurrenceDurationTypes {
+   SpecificDaysOfWeek,
+   PerNumberOfDays,
+   PerNumberOfWeeks,
+   Weekly = 'Week',
+   Monthly = 'Month',
+   Quarterly = 'Quarter',
+}
+
+export const recurrenceToNumberOfDaysMap: Map<RecurrenceDurationTypes, number> = new Map([
+   [RecurrenceDurationTypes.Weekly, 7],
+   [RecurrenceDurationTypes.Monthly, 31],
+   [RecurrenceDurationTypes.Quarterly, 90],
+]);
+
+const DaysOfWeekToString = (target: DaysOfWeek[]) => {
+   return target
+      .sort()
+      .map((m) => dayjs().day(m).format('dd'))
+      .join(' ');
+};
+
+/**
+ * Hook to get a display string that represents the recurrence
+ * TODO: move to seperate file, make a test
+ * @param aggregationPeriod
+ * @param durationType
+ * @param target
+ */
+export const useRecurrenceString = (
+   aggregationPeriod: RecurrenceAggregationPeriods,
+   durationType: RecurrenceDurationTypes,
+   target: number | DaysOfWeek[]
+): string => {
+   const intl = useIntl();
+
+   if (aggregationPeriod === RecurrenceAggregationPeriods.TotalPerDay) {
+      if (
+         typeof target === 'number' &&
+         (durationType === RecurrenceDurationTypes.Weekly ||
+            durationType === RecurrenceDurationTypes.Monthly ||
+            durationType === RecurrenceDurationTypes.Quarterly)
+      ) {
+         return intl.formatMessage(
+            {
+               defaultMessage: '{target} {target, plural, one {day} other {days}}/{durationType}',
+            },
+            { target, durationType }
+         );
+      } else if (durationType === RecurrenceDurationTypes.SpecificDaysOfWeek && Array.isArray(target)) {
+         // naive logic to know its all days of the week
+         if (target.length === 7) {
+            return intl.formatMessage({
+               defaultMessage: 'Every day',
+            });
+         } else {
+            const selectedWeekdays = DaysOfWeekToString(target);
+            return selectedWeekdays;
+         }
+      } else if (durationType === RecurrenceDurationTypes.PerNumberOfDays && typeof target == 'number') {
+         return intl.formatMessage(
+            {
+               defaultMessage: 'Every {target} days',
+            },
+            { target }
+         );
+      }
+   } else if (aggregationPeriod === RecurrenceAggregationPeriods.TotalPerWeek) {
+      if (typeof target === 'number' && (durationType === RecurrenceDurationTypes.Monthly || durationType === RecurrenceDurationTypes.Quarterly)) {
+         return intl.formatMessage(
+            {
+               defaultMessage: '{target} {target, plural, one {week} other {weeks}}/{durationType}',
+            },
+            { target, durationType }
+         );
+      } else if (durationType === RecurrenceDurationTypes.PerNumberOfWeeks && typeof target == 'number') {
+         return intl.formatMessage(
+            {
+               defaultMessage: 'Every {target} weeks',
+            },
+            { target }
+         );
+      }
+   } else if (aggregationPeriod === RecurrenceAggregationPeriods.TotalPerMonth) {
+      if (typeof target === 'number' && durationType === RecurrenceDurationTypes.Quarterly) {
+         return intl.formatMessage(
+            {
+               defaultMessage: '{target} {target, plural, one {month} other {months}}/{durationType}',
+            },
+            { target, durationType }
+         );
+      }
+   }
+
+   throw createException(ExceptionTypes.Schedule_RecurrenceConfigurationIsInvalid, { recurrence: durationType });
+};
--- src/components/routes/goal-details/initatives/attributes/scheduleDefinitions.ts
+++ src/components/routes/goal-details/initatives/attributes/scheduleDefinitions.ts
@@ -2,8 +2,6 @@
  * Match the Dayjs standard
  */
 
-import { createException, ExceptionTypes } from '~~/models/Exception';
-
 export enum DaysOfWeek {
    Sunday = 0,
    Monday = 1,
@@ -20,39 +18,3 @@ export enum QuartersOfYear {
    Q3 = 3,
    Q4 = 4,
 }
-
-export enum CycleDurationTypes {
-   PerDay = 'day',
-   PerWeek = 'week',
-   PerMonth = 'month',
-   PerQuarter = 'quarter',
-}
-
-export enum RecurrenceTypes {
-   SpecificDaysOfWeek,
-   TotalPerDay,
-   TotalPerWeek,
-   TotalPerMonth,
-   TotalPerQuarter,
-   PerNumberOfDays,
-}
-
-export const recurrenceToNumberOfDaysMap: Map<RecurrenceTypes, number> = new Map([
-   [RecurrenceTypes.DaysPerWeek, 7],
-   [RecurrenceTypes.DaysPerMonth, 31],
-   [RecurrenceTypes.DaysPerQuarter, 90],
-]);
-
-export const recurrenceToDisplayString = (cycleDuration: CycleDurationTypes, recurrence: RecurrenceTypes, data: number): string => {
-   const recurrenceToDisplayMap: Map<RecurrenceTypes, string> = new Map([
-      [RecurrenceTypes.PerNumberOfDays, `Every ${data} days`],
-      [RecurrenceTypes.TotalPerMonth, `${data} times a ${cycleDuration}`],
-      [RecurrenceTypes.totalper, `${data} times a month`],
-      [RecurrenceTypes.TotalPerMonth, `${data} times a quarter`],
-   ]);
-
-   const result = recurrenceToDisplayMap.get(recurrence);
-
-   if (result) return result;
-   else throw createException(ExceptionTypes.Schedule_RecurrenceUseIsInvalid, { recurrence });
-};

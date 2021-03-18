58f714bb699eba2f6ab06769ba4f8268b72804e5
twrobel <tomasz.wrobel@gmail.com>
2021-03-17 20:45:46 -0400

    Adding trend chart

---
+++ src/components/routes/goal-details/charts/TrendChart.tsx
@@ -0,0 +1,54 @@
+import { ResponsiveLine, Datum } from '@nivo/line';
+import React from 'react';
+import { ErrorBoundary, ErrorFallback } from '~~/components/common/ErrorFallback';
+
+import { useTheme } from '@material-ui/core';
+
+interface ITrendChartProps {
+   title: string;
+   dataPoints: Datum[];
+}
+
+export const TrendChart = ({ title, dataPoints }: ITrendChartProps): JSX.Element => {
+   const theme = useTheme();
+   const data = [
+      {
+         id: title,
+         color: '#e8c1a0',
+         data: dataPoints,
+      },
+   ];
+   const tickConfig = {
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+   };
+   return (
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+         <div className="flex w-full overflow-hidden h-80" css={[{ borderColor: theme.palette.text.secondary }]}>
+            <ResponsiveLine
+               data={data}
+               margin={{ top: 50, right: 30, bottom: 60, left: 50 }}
+               xScale={{ type: 'point' }}
+               yScale={{ type: 'linear', min: 0, max: 'auto', stacked: true, reverse: false }}
+               yFormat=" >-.2f"
+               gridYValues={[10, 20, 30, 40]}
+               axisTop={null}
+               lineWidth={5}
+               axisRight={null}
+               axisBottom={{ ...tickConfig, orient: 'bottom' }}
+               axisLeft={{ ...tickConfig, orient: 'left' }}
+               enableArea={true}
+               enableGridX={false}
+               pointSize={4}
+               pointColor={{ theme: 'background' }}
+               pointBorderWidth={6}
+               pointBorderColor={{ from: 'serieColor' }}
+               pointLabelYOffset={-12}
+               isInteractive={false}
+               legends={[]}
+            />
+         </div>
+      </ErrorBoundary>
+   );
+};

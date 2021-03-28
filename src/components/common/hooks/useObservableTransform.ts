9dfd20e81dfab9f19f8e849c8e55305da19a6d2c
Shravan Sunder <shravan.retail@gmail.com>
2021-03-27 22:32:38 -0400

    [ch155] 📦 🚧 rxjs is not working

--- src/components/common/hooks/useObservableTransform.ts
+++ src/components/common/hooks/useObservableTransform.ts
@@ -2,27 +2,27 @@ import { identity, Observable } from 'rxjs';
 import { useObservableCallback, useObservableState, useSubscription } from 'observable-hooks';
 import { useEffect } from 'react';
 import { ObservableWithValue } from './useObservableValue';
-import { RecoilState, useRecoilState, useSetRecoilState } from 'recoil';
 
-export type TOperator<T1, T2> = (o1$: Observable<T1>, o2$: Observable<T2>) => Observable<T1>;
+export type TOperator<T1> = (o1$: Observable<T1>, ...otherObservables$: [Observable<any>]) => Observable<T1>;
 /**
  * Creates and returns an ObservableWithState from initalValue
  * The observable has a operator applied to it.
  * @param initValue
- * @param second$ second observable input to the operator
+ * @param otherObservables$ second observable input to the operator
  * @param operator Accepts two arguments, 1. the current observable, 2. second input observable
  */
 
-export const useObservableTransform = <T1, T2>(initValue: T1, second$: Observable<T2>, operator: TOperator<T1, T2>): ObservableWithValue<T1> => {
-   const [push, observable$] = useObservableCallback<T1>((event$) => operator(event$, second$));
+export const useObservableTransform = <T1>(initValue: T1, operator: TOperator<T1>, ...otherObservables$: [Observable<any>]): ObservableWithValue<T1> => {
+   const [push, observable$] = useObservableCallback<T1>((event$) => operator(event$, ...otherObservables$));
    const value = useObservableState(observable$, initValue);
 
    useEffect(() => {
       push(initValue);
-   }, [push, observable$, second$]);
       console.log('effect transform');
       // eslint-disable-next-line react-hooks/exhaustive-deps
    }, []);
+
+   useSubscription(observable$, (e) => console.log(e));
 
    return { observable$: observable$, push: push, value };
 };
--- src/components/routes/goal-details/initatives/attributes/core/useInitiativeSchedule.tsx
+++ src/components/routes/goal-details/initatives/attributes/core/useInitiativeSchedule.tsx
@@ -5,6 +5,7 @@ import { useObservableTransform, TOperator } from '~~/components/common/hooks/us
 import { map } from 'rxjs/operators';
 import { combineLatest } from 'rxjs';
 import { updateDuration } from './recurrenceLogic';
+import { useSubscription } from 'observable-hooks';
 
 export interface IRecurrenceObservables {
    aggregationPeriod: ObservableWithValue<RecurrenceAggregationPeriods>;
@@ -15,13 +16,10 @@ export interface IRecurrenceObservables {
 export const useRecurrenceObservables = (): IRecurrenceObservables => {
    const aggregationPeriod = useObservableValue<RecurrenceAggregationPeriods>(RecurrenceAggregationPeriods.PerDay);
 
-   const durationOperator: TOperator<RecurrenceDurationTypes, RecurrenceAggregationPeriods> = (o1$, o2$) =>
-      combineLatest([o1$, o2$]).pipe(map(([state1, state2]) => updateDuration(state1, state2)));
-   const durationType = useObservableTransform<RecurrenceDurationTypes, RecurrenceAggregationPeriods>(
-      RecurrenceDurationTypes.Weekly,
-      aggregationPeriod.observable$,
-      durationOperator
-   );
+   const durationOperator: TOperator<RecurrenceDurationTypes> = (o1$, o2$) => o2$.pipe(map((x) => RecurrenceDurationTypes.Monthly));
+   const durationType = useObservableTransform<RecurrenceDurationTypes>(RecurrenceDurationTypes.Weekly, durationOperator, aggregationPeriod.observable$);
+
+   useSubscription(durationType.observable$, (e) => console.log(e));
 
    const target = useObservableValue<number | DaysOfWeek[]>(5);
 

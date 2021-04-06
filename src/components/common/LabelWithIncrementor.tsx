a74bbff9bbc0bab682e34d229c1fa7e49047ea17
Shravan Sunder <shravan.retail@gmail.com>
2021-04-05 22:05:23 -0400

    [ch176]  ✨🎨 New created label with incrementor

---
+++ src/components/common/LabelWithIncrementor.tsx
@@ -0,0 +1,33 @@
+import { css } from '@emotion/react';
+import { Button, ButtonGroup, Typography } from '@material-ui/core';
+import { ArrowDropDown } from '@material-ui/icons';
+import React, { FC, MouseEvent } from 'react';
+import tw from 'twin.macro';
+import { ICommonProps } from '~~/components/common/ICommonProps';
+import { LabelContainer } from '~~/components/common/LabelContainer';
+import { combine } from '~~/helpers/string';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+
+interface ILabelWithStepperProps extends ICommonProps {
+   title: string;
+   handleClick: (event: MouseEvent) => void;
+   showing?: boolean;
+}
+
+export const LabelWithStepper: FC<ILabelWithStepperProps> = (props) => {
+   return (
+      <LabelContainer className={props.className} css={[props.showing ? css({}) : css(tw`bg-gradient-to-r from-transparent to-gray-50`)]} title={props.title}>
+         <div className="flex flex-grow-0  w-10 bg-gray-200 border-l-1">
+            <ButtonGroup color="primary" aria-label="Numeric incrementor and decrementor group">
+               <Button className=" w-10 min-w-0 " onClick={(event: MouseEvent) => props.handleClick(event)}>
+                  <AddIcon />
+               </Button>
+               <Button className=" w-10 min-w-0 " onClick={(event: MouseEvent) => props.handleClick(event)}>
+                  <RemoveIcon />
+               </Button>
+            </ButtonGroup>
+         </div>
+      </LabelContainer>
+   );
+};
